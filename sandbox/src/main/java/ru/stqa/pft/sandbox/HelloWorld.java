7777082ed334bcea6fc770cc35b689223d6fcd63
forilyam <For_ilya_m@mail.ru>
2019-03-16 21:59:35 +0500

    Создан класс для вычисления расстояния между точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -3,6 +3,13 @@ package ru.stqa.pft.sandbox;
 public class HelloWorld {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+
+        Point a = new Point(1,2);
+        Point b = new Point(4,5);
+
+        System.out.println(a.distance(a,b));
     }
+
 }
